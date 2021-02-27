877b0b1a4a2544ac1d037ab1f41c5fb3450ca17c
Teaapoo <danielliu385@gmail.com>
2021-02-26 20:40:58 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-$.getJSON( "pew",function(data){
+$.getJSON("https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/Teaapoo/Yeet?api_key=RGAPI-4283bcc7-3da2-4588-8d79-a8d0e77127ef",function(data){
     console.log(data);
 
 
