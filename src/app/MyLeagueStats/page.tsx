96ba7fc90bea6c165e62e3a8c4e497ea20cc72dc
Teaapoo <dliu224@my.yorku.ca>
2025-01-29 00:01:05 -0500

    Create page.tsx

---
+++ src/app/MyLeagueStats/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import axios from 'axios';
+import 'dotenv/config';
+import { use, useState } from 'react';
+
+
+interface PlayerData{
+    id: string;
+    name: string;
+}
+
+
+interface RankData{
+    
+}
+
+
+export default function MyLeaugeStats(){
+    const [summonerName,setSummonerName] = useState(''); 
+    const [playerData, setPlayerData] = useState<PlayerData | null>(null);
+    const [rankedData, setRankedData] = useState<RankData[]>([]);
+
+
+    return(
+        <div>
+            Bello!
+        </div>
+    );
+}
+
+
+
+
+
